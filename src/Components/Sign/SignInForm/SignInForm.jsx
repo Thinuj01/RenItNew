1b2aa451ec548e4004b49bc55210b2b107f3e305
Thinuja Hettiarachchi <100994721+Thinuj01@users.noreply.github.com>
2024-08-18 01:17:39 +0530

    backendUpdated

--- src/Components/Sign/SignInForm/SignInForm.jsx
+++ src/Components/Sign/SignInForm/SignInForm.jsx
@@ -1,40 +1,81 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './SignInForm.css'
-import { Link } from 'react-router-dom'
+import { Link,useNavigate } from 'react-router-dom'
+import axios from 'axios'
 
 function SignInForm() {
+  const [userName, setUserName] = useState('');
+  const [userPswd, setUserPswd] = useState('');
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const status = '1';
+    console.log(status);
+    axios.post('http://localhost:4433/RentIT/Controllers/userLoginController.php', {
+      userName: userName,
+      userPswd: userPswd,
+      status: status,
+    }, {
+      withCredentials: true
+    })
+      .then(response => {
+        console.log(response.data);
+        if(response.data == "Account Validated"){
+            alert("Login Successfull");
+            navigate('/');
+        }else{
+            alert("Invalid Username or Password");
+        }
+
+      })
+      .catch(error => {
+        console.error('There was an error signing in!', error);
+      });
+  };
+
   return (
-    <>
-        <div id="signInFormContainer">
-            <div className="signInTitle">
-                Sign in
+    <div id="signInFormContainer">
+      <div className="signInTitle">
+        Sign in
+      </div>
+      <div className="signInForm">
+        <form onSubmit={handleSubmit}>
+          <div className="name_pswd">
+            <input
+              type="text"
+              name='userName'
+              placeholder='Email or NIC'
+              value={userName}
+              onChange={(e) => setUserName(e.target.value)}
+            />
+            <input
+              type="password"
+              name="userPswd"
+              placeholder='Password'
+              value={userPswd}
+              onChange={(e) => setUserPswd(e.target.value)}
+            />
+          </div>
+
+          <div className="remember_fogot">
+            <div className="remberDiv">
+              <input type="checkbox" name="remberMe" id="" />
+              <label htmlFor="remberMe">Remember me</label>
             </div>
-            <div className="signInForm">
-                <form action="">
-                    <div className="name_pswd">
-                        <input type="text" name='userName' placeholder='Email or NIC'/>
-                        <input type="password" name="userPswd" placeholder='Password' id="" />
-                    </div>
-
-                    <div className="remember_fogot">
-                        <div className="remberDiv">
-                            <input type="checkbox" name="remberMe" id="" />
-                            <label htmlFor="remberMe">Rember me</label>
-                        </div>
-                        <div className="forgotDiv">
-                            <Link to=''>Forgot password</Link>
-                        </div>
-                    </div>
-
-                    <input type="submit" name='submit' value="Sign in"/>
-
-                    <div className="signUpQ">
-                        <p>Don't you have an account? <Link to="/signup">Sign up</Link></p>
-                    </div>
-                </form>
+            <div className="forgotDiv">
+              <Link to=''>Forgot password</Link>
             </div>
-        </div>
-    </>
+          </div>
+
+          <input type="submit" name='submit' value="Sign in" />
+
+          <div className="signUpQ">
+            <p>Don't you have an account? <Link to="/signup">Sign up</Link></p>
+          </div>
+        </form>
+      </div>
+    </div>
   )
 }
 
