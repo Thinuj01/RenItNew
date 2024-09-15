37a03aa9a40ed3f175f66d04175f94bae6bf2b65
Ravindu Dilshan Karunathilaka <cst21031@std.uwu.ac.lk>
2024-09-15 21:08:44 +0530

    /PuchasePage

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { createBrowserRouter, RouterProvider, } from "react-router-dom";
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
@@ -8,24 +9,22 @@ import SignUpPage from './Components/Sign/SignUpPage/SignUpPage.jsx'
 import ForgotPswdPage from './Components/Sign/ForgotPswdPage/ForgotPswdPage.jsx'
 import AddItemPage from './Components/AddItemPage/AddItemPage.jsx'
 import ItemPreviewPage from './Components/ItemPreviewPage/ItemPreviewPage.jsx'
-
-import {createBrowserRouter, RouterProvider,} from "react-router-dom";
 import CategoryViewPage from './Components/CategoryViewPage/CategoryViewPage.jsx'
-
 import ItemImageSlider from './Components/ItemImageSlider/ItemImageSlider.jsx'
 import FeedBackShowingBox from './Components/FeedBackShowingBox/FeedBackShowingBox.jsx'
-
+import PurchasePage from './Components/PurchasePage/PurchasePage.jsx'
 
 const router = createBrowserRouter([
-  { path: "/", element: <App/>, },
-  { path: "/signin", element: <SignInPage/>, },
-  { path: "/signup", element: <SignUpPage/>, },
-  { path: "/forgotpswd", element: <ForgotPswdPage/>, },
-  { path: "/additempage", element: <AddItemPage/>, },
-  { path: "/ItemPreviewPage", element: <ItemPreviewPage/>, },
-  { path: "/CategoryViewPage", element: <CategoryViewPage/>, },
-  { path: "/ItemImageSlider", element: <ItemImageSlider/>, },
-  { path: "/FeedBackShowingBox", element: <FeedBackShowingBox/>, },
+  { path: "/", element: <App />, },
+  { path: "/signin", element: <SignInPage />, },
+  { path: "/signup", element: <SignUpPage />, },
+  { path: "/forgotpswd", element: <ForgotPswdPage />, },
+  { path: "/additempage", element: <AddItemPage />, },
+  { path: "/ItemPreviewPage", element: <ItemPreviewPage />, },
+  { path: "/CategoryViewPage", element: <CategoryViewPage />, },
+  { path: "/ItemImageSlider", element: <ItemImageSlider />, },
+  { path: "/FeedBackShowingBox", element: <FeedBackShowingBox />, },
+  { path: "/PurchasePage", element: <PurchasePage />, },
 
 ]);
 
